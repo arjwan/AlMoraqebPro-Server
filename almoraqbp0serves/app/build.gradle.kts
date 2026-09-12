abeb41051d9df994fe747ff97cc8e98467c9e099
arjwan <arjwan307@gmail.com>
2026-09-12 21:27:59 +0300

    Bump Android app to 4.0.3

--- almoraqbp0serves/app/build.gradle.kts
+++ almoraqbp0serves/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mohmmedali.almoraqebpro"
         minSdk = 26
         targetSdk = 34
-        versionCode = 27
-        versionName = "4.0.2"
+        versionCode = 28
+        versionName = "4.0.3"
         multiDexEnabled = true
     }
 
